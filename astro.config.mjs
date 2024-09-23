39cdea1c72717305b19e5a1eff104c6cd1b042b8
Gustavo Oliveira <dogustavo.oliveira@gmail.com>
2024-09-23 16:21:01 -0300

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid',
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+          additionalData: `
+            @import "src/styles/app";
+          `
+        }
+      }
+    }
+  }
+})
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface Window {
+  theme: {
+    setTheme: (theme: 'dark' | 'light') => void
+    getTheme: () => 'dark' | 'light'
+  }
+}
